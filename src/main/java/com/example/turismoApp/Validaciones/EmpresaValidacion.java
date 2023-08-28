a06f2ff510e8fd34be50389b29702f73f1903931
Your Name <you@example.com>
2023-08-28 15:54:51 -0500

    ok

---
+++ src/main/java/com/example/turismoApp/Validaciones/EmpresaValidacion.java
@@ -0,0 +1,17 @@
+package com.example.turismoApp.Validaciones;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class EmpresaValidacion {
+
+
+    public Boolean validarNombre(String nombre) throws Exception{
+        if (nombre.length()>30){
+            return false;
+        }else{
+            return true;
+        }
+    }
+
+}
